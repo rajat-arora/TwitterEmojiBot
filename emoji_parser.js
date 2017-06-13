2d10561678adacfbfded6a5df8d6c759addd8d20
rajat-arora <rajat-arora@users.noreply.github.com>
2017-06-13 11:15:35 -0400

    made improvements

--- emoji_parser.js
+++ emoji_parser.js
@@ -1,11 +1,10 @@
 var emoji = {};
-var SYMBOLS = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
-var XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest;
-
-var allEmojis;
+const SYMBOLS = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
+const emojilib = require("emojilib");
+const allEmojis = emojilib.lib;
 
 emoji.translate  = function(words){
-    allEmojis = require('./emoji.json')
+   // allEmojis = require('./emoji.json')
   
     string = ''
     words = words.split(/(\s+)/);
@@ -16,8 +15,6 @@ emoji.translate  = function(words){
     }
     return string;
 }
-
-
 
 
 emoji.translateWord = function(word){
@@ -40,36 +37,69 @@ emoji.translateWord = function(word){
 
 }
 
+emoji.isMaybeAlreadyEmoji = function(word){
+  let ranges = [
+      '\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
+      '\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
+      '\ud83d[\ude80-\udeff]' // U+1F680 to U+1F6FF
+  ];
+
+  return word.match(ranges.join('|')) != null;
+}
+
 
 emoji.giveMeAEmoji = function(word){
-    keyword = word.trim().toLowerCase();
+    let keyword = word.trim().toLowerCase();
 
-    if(!keyword || keyword === '' || keyword === 'it'){
+      if (!keyword || keyword === '' || keyword === 'a' || keyword === 'it' || keyword === 'is'){
         return '';
     };
 
       // Maybe this is a plural word but the keyword is the singular?
-  var maybeSingular = '';
+  let maybeSingular = '';
   if (keyword[keyword.length - 1] == 's')
     maybeSingular = keyword.slice(0, keyword.length - 1);
 
   // Maybe this is a singular word but the keyword is the plural?
   // Don't do this for single letter since that will pluralize crazy things.
-  var maybePlural = (keyword.length == 1) ? '' : keyword + 's';
+  let maybePlural = (keyword.length == 1) ? '' : keyword + 's';
+  let maybeVerbed = (word.indexOf('ing') == -1) ? '' : word.substr(0, word.length-3);
+
+
+  if (emoji.isMaybeAlreadyEmoji(word)) return word;
+
+
+   // If it's "i" or "i", add some faces to it.
+  if (word === 'i' || word === 'you') {
+    return '😀';
+    //return '😊';
+  } else if (word === 'she'){
+    return '💁';
+    //for some reason IDK why osx thinks the emoji for a guy is a woman with a key _/-_-\_
+  } else if (word === 'he'){
+    return '💁‍♂️';
+  } else if (word === 'we' || word === 'they') {
+    return '👩‍👩‍👦‍👦';
+  } else if (word === 'am' || word === 'is' || word === 'are') {
+    return '👉';
+  }
 
   // Go through all the things and find the first one that matches.
-  for (var emoji in allEmojis) {
-    var keywords = allEmojis[emoji].keywords;
-    if (emoji == keyword || emoji == maybeSingular || emoji == maybePlural ||
-        (keywords && keywords.indexOf(keyword) >= 0) ||
+  for (let emoji in allEmojis) {
+    //console.log("here")
+    let keywords = allEmojis[emoji].keywords;
+    if (word == allEmojis[emoji].char || emoji == word || (emoji == word + '_face')||
+    emoji == maybeSingular || emoji == maybePlural || emoji == maybeVerbed
+         ||(keywords && keywords.indexOf(word) >= 0) ||
         (keywords && keywords.indexOf(maybeSingular) >= 0) ||
-        (keywords && keywords.indexOf(maybePlural) >= 0))
-      return allEmojis[emoji].char;
-  }
+        (keywords && keywords.indexOf(maybePlural) >= 0) ||
+        (keywords && keywords.indexOf(maybeVerbed) >= 0) ){
+          if (!(word.length <= 3 && allEmojis[emoji].category == 'flags')){
+         return allEmojis[emoji].char
+          }
+  } 
+}
   return word;
-
-
-
 
 };
 
